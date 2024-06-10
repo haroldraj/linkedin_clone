7d1e2e97a1038b80197b5dba3471732f665b69be
takkamdarios <takkamdarios@gmail.com>
2024-06-10 19:49:11 +0200

    Header updated

--- web/src/components/Common/Header.jsx
+++ web/src/components/Common/Header.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import './Header.module.css';
 
-function Header() {
+const Header = () => {
     return (
-        <header className="header">
-            <img src="/assets/linkedin_logo.png" alt="LinkedIn Logo" className="logo" />
-            <nav>
-                <ul className="nav-links">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/new-post">New Post</a></li>
-                    <li><a href="/profile">Profile</a></li>
-                </ul>
-            </nav>
-            <button className="logout-button">Logout</button>
-        </header>
+        <Navbar bg="light" expand="lg" className="header">
+            <LinkContainer to="/">
+                <Navbar.Brand>LinkedIn Clone</Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <LinkContainer to="/">
+                        <Nav.Link>Home</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/profile">
+                        <Nav.Link>Profile</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/new-post">
+                        <Nav.Link>New Post</Nav.Link>
+                    </LinkContainer>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-success">Search</Button>
+                </Form>
+            </Navbar.Collapse>
+        </Navbar>
     );
-}
+};
 
 export default Header;
