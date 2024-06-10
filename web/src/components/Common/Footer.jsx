cb68a4a89aa87ed7388f9e2c1b0f6cfc811d7700
takkamdarios <takkamdarios@gmail.com>
2024-06-10 19:58:16 +0200

    Layout adjustments

--- web/src/components/Common/Footer.jsx
+++ web/src/components/Common/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Form } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import './Footer.module.css';
 
 const Footer = () => {
@@ -7,82 +7,34 @@ const Footer = () => {
         <footer className="footer">
             <Container>
                 <Row>
-                    <Col xs={12} md={6} lg={3}>
-                        <a href="#">Informazioni</a>
-                    </Col>
-                    <Col xs={12} md={6} lg={3}>
-                        <a href="#">Linee guida della community</a>
-                    </Col>
-                    <Col xs={12} md={6} lg={3}>
-                        <a href="#">Privacy e condizioni</a>
-                    </Col>
-                    <Col xs={12} md={6} lg={3}>
-                        <a href="#">Sales solutions</a>
+                    <Col>
+                        <p>LinkedIn Corporation &copy; {new Date().getFullYear()}</p>
                     </Col>
                 </Row>
                 <Row>
                     <Col xs={12} md={6} lg={3}>
-                        <a href="#">Centro sicurezza</a>
+                        <a href="/">About</a>
                     </Col>
                     <Col xs={12} md={6} lg={3}>
-                        <a href="#">Accessibilità</a>
+                        <a href="/">Community Guidelines</a>
                     </Col>
                     <Col xs={12} md={6} lg={3}>
-                        <a href="#">Carriera</a>
+                        <a href="/">Privacy & Terms</a>
                     </Col>
                     <Col xs={12} md={6} lg={3}>
-                        <a href="#">Opzioni di annuncio</a>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={12} md={6} lg={3}>
-                        <a href="#">Mobile</a>
+                        <a href="/">Sales Solutions</a>
                     </Col>
                     <Col xs={12} md={6} lg={3}>
-                        <a href="#">Talent Solutions</a>
+                        <a href="/">Safety Center</a>
                     </Col>
                     <Col xs={12} md={6} lg={3}>
-                        <a href="#">Soluzioni di marketing</a>
+                        <a href="/">Careers</a>
                     </Col>
                     <Col xs={12} md={6} lg={3}>
-                        <a href="#">Pubblicità</a>
+                        <a href="/">Advertising</a>
                     </Col>
-                </Row>
-                <Row className="footer-settings">
-                    <Col xs={12} md={6} lg={3} className="d-flex flex-row">
-                        <i className="bi bi-question-circle-fill mx-0 mt-2"></i>
-                        <div className="ml-2">
-                            <a href="#">Domande?</a>
-                            <p>Visita il nostro Centro assistenza.</p>
-                        </div>
-                    </Col>
-                    <Col xs={12} md={6} lg={3} className="d-flex flex-row">
-                        <i className="bi bi-gear-fill mx-0 mt-2"></i>
-                        <div className="ml-2">
-                            <a href="#">Gestisci il tuo account e la tua privacy</a>
-                            <p>Vai alle impostazioni</p>
-                        </div>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={12} className="mt-4 text-center">
-                        <Form>
-                            <Form.Group controlId="languages">
-                                <Form.Label>Seleziona lingua</Form.Label>
-                                <Form.Control as="select">
-                                    <option>Inglese (English)</option>
-                                    <option>Italiano (Italian)</option>
-                                    <option>Russo (Russian)</option>
-                                    <option>Francese (French)</option>
-                                    <option>Tedesco (German)</option>
-                                </Form.Control>
-                            </Form.Group>
-                        </Form>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col className="mt-4 text-center">
-                        <p>linkedin corporation &copy; {new Date().getFullYear()}</p>
+                    <Col xs={12} md={6} lg={3}>
+                        <a href="/">Small Business</a>
                     </Col>
                 </Row>
             </Container>
--- web/src/components/Common/Header.jsx
+++ web/src/components/Common/Header.jsx
@@ -1,32 +1,42 @@
 import React from 'react';
-import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { HouseDoorFill, PeopleFill, BriefcaseFill, ChatDotsFill, BellFill } from 'react-bootstrap-icons';
 import './Header.module.css';
 
-const Header = () => {
+const Header = ({ userProfile }) => {
     return (
         <Navbar bg="light" expand="lg" className="header">
-            <LinkContainer to="/">
-                <Navbar.Brand>LinkedIn Clone</Navbar.Brand>
-            </LinkContainer>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <LinkContainer to="/">
-                        <Nav.Link>Home</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/profile">
-                        <Nav.Link>Profile</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/new-post">
-                        <Nav.Link>New Post</Nav.Link>
-                    </LinkContainer>
-                </Nav>
-                <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-success">Search</Button>
-                </Form>
-            </Navbar.Collapse>
+            <Link to="/" className="navbar-brand">
+                <img
+                    src="/logo192.png"
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                    alt="LinkedIn logo"
+                />
+            </Link>
+            <Form inline className="mr-auto">
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-primary">Search</Button>
+            </Form>
+            <Nav className="ml-auto">
+                <Link to="/" className="nav-link"><HouseDoorFill size={22} /> Home</Link>
+                <Link to="/network" className="nav-link"><PeopleFill size={22} /> My Network</Link>
+                <Link to="/jobs" className="nav-link"><BriefcaseFill size={22} /> Jobs</Link>
+                <Link to="/messaging" className="nav-link"><ChatDotsFill size={22} /> Messaging</Link>
+                <Link to="/notifications" className="nav-link"><BellFill size={22} /> Notifications</Link>
+                <Link to={`/profile/${userProfile.id}`} className="nav-link">
+                    <img
+                        src={userProfile.image}
+                        width="30"
+                        height="30"
+                        className="rounded-circle"
+                        alt="Profile"
+                    />
+                    Me
+                </Link>
+            </Nav>
         </Navbar>
     );
 };
