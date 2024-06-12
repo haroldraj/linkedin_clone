ce0b78696c6b193f1cae8fae364c08e1131a452f
Harold <rnarsonharold@gmail.com>
2024-06-12 19:06:22 +0200

    add static pages

---
+++ web/src/pages/MessagingPage.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/header/Header";
+
+export default function MessagingPage() {
+  return (
+      <div>
+          <Header />
+          <div className="text-3xl text-center">
+              MessagingPage
+          </div></div>
+  )
+}
---
+++ web/src/pages/NetworkPage.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/header/Header";
+
+export default function NetworkPage() {
+  return (
+      <div>
+          <Header />
+          <div className="text-3xl text-center">
+              NetworkPage
+          </div></div>
+  )
+}
