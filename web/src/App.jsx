4d65781851a8ef6fe71b9c24b1615bb03192c2a5
takkamdarios <takkamdarios@gmail.com>
2024-06-12 08:41:39 +0200

    app and client related to authservice adjusted

---
+++ web/src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+import NewPostForm from './components/NewPost/NewPostForm';
+import UserProfile from './components/Profile/UserProfile';
+import Home from './components/Home/Home';
+import './App.module.css';
+
+function App() {
+    const [userProfile, setUserProfile] = useState({});
+
+    return (
+        <Router>
+            <Header userProfile={userProfile} />
+            <div className="app-container">
+                <Routes>
+                    <Route path="/" element={<Home userProfile={userProfile} setUserProfile={setUserProfile} />} />
+                    <Route path="/profile/:id" element={<UserProfile userProfile={userProfile} setUserProfile={setUserProfile} />} />
+                    <Route path="/new-post" element={<NewPostForm />} />
+                </Routes>
+            </div>
+            <Footer />
+        </Router>
+    );
+}
+
+export default App;
---
+++ web/src/utils/apiClient.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+    baseURL: 'http://localhost:8080/api', // Base URL for the backend API
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+apiClient.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+export default apiClient;
